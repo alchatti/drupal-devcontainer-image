edfddfe5d2605f196e682119124b228a535ef13f
Majed Al-Chatti <majed@alchatti.com>
2024-01-11 12:25:24 +0400

    build: 🧑‍💻 Added `env` vars for testing
    -e "TESTING_1=⭐⭐⭐Hello World!⭐⭐⭐" \
    -e "TESTING_2=🚀🛸🛰️🕳️💫" \

--- test.sh
+++ test.sh
@@ -5,4 +5,6 @@ docker run --rm -it \
 -u "vscode" \
 -p 80:80 \
 -v "drupal-dev-html:/var/www/html" \
+-e "TESTING_1=⭐⭐⭐Hello World!⭐⭐⭐" \
+-e "TESTING_2=🚀🛸🛰️🕳️💫" \
 local/drupal-devcontainer:latest fish
